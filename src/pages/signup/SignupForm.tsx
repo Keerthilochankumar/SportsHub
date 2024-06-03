25391d29cfc738fc8aea848643ab2767ddb2de5f
keerthilochankumar <keerthi.lochans@gmail.com>
2024-06-03 21:29:25 +0530

    updated form

--- src/pages/signup/SignupForm.tsx
+++ src/pages/signup/SignupForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Button } from '@headlessui/react'
 const SignupForm: React.FC = () => {
@@ -9,12 +9,13 @@ const SignupForm: React.FC = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const response = await fetch(`https://wd301-api.pupilfirst.school/users`, {
+      console.log(process.env.VITE_API_ENDPOINT , "this is the env")
+      const response = await fetch(`${import.meta.env.SERVER_ENDPOINT}/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({name: userName , email: userEmail , password: userPassword}),
       });
-
+      console.log(response , "response")
       if (!response.ok) {
         throw new Error('Sign-up failed');
       }
@@ -26,28 +27,86 @@ const SignupForm: React.FC = () => {
       toast.error("error while signing in");
     }
   };
-return (
-    <div className="w-full h-screen">
-      <div className="flex flex-col">
-        <div className="min-w-[400px] min-h-[600px] border-2 border-white rounded-xl">
-          <div className="flex flex-col">
-            <label>User Name</label>
-            <input type="text" name="userName" onChange={(e)=>{setUserName(e.target.value)}}/>
+
+  useEffect(()=>{
+    console.log(userEmail , userName , userPassword);
+  },[userEmail , userPassword , userName])
+
+  return (
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  min-w-[500px] mt-10">
+        <div className="w-full bg-white bg-gray-200/50 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-black">
+              Sign Up
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">
+                  User Name
+                </label>
+                <input
+                  type="text"
+                  name="user name"
+                  id="user name"
+                  className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  required
+                  onChange={(e)=>{setUserName(e.target.value)}}
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                  onChange={(e)=>{setUserEmail(e.target.value)}}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                  onChange={(e)=>{setUserPassword(e.target.value)}}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                  </div>
+                </div>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Forgot password?
+                </a>
+              </div>
+              <Button
+                type="submit"
+                className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border-gray-900 border-2"
+              >
+                Sign Up
+              </Button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                have an account ?{' '}
+                <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Sign In
+                </a>
+              </p>
+            </form>
           </div>
-          <div className="flex flex-col">
-            <label>Email</label>
-            <input type="text" name="userName" onChange={(e)=>{setUserEmail(e.target.value)}}/>
-          </div>
-          <div className="flex flex-col">
-            <label>Password</label>
-            <input type="text" name="userName" onChange={(e)=>{setUserPassword(e.target.value)}}/>
-          </div>
-          <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
-             Sign Up
-          </Button>
         </div>
       </div>
-    </div>
   );
 };
 
