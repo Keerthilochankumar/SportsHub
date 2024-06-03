65b0e379dbd15a36ce2718e1c239c018c130012e
keerthilochankumar <keerthi.lochans@gmail.com>
2024-06-03 20:32:18 +0530

    changes to sign up

--- src/pages/signup/SignupForm.tsx
+++ src/pages/signup/SignupForm.tsx
@@ -1,50 +1,53 @@
 import React, { useState } from 'react';
-
+import { toast } from 'react-toastify';
+import { Button } from '@headlessui/react'
 const SignupForm: React.FC = () => {
-  const [organisationName, setOrganisationName] = useState('');
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     try {
-      const response = await fetch(`https://wd301-api.pupilfirst.school/organisations`, {
+      const response = await fetch(`https://wd301-api.pupilfirst.school/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: organisationName, user_name: userName, email: userEmail, password: userPassword}),
+        body: JSON.stringify({name: userName , email: userEmail , password: userPassword}),
       });
 
       if (!response.ok) {
         throw new Error('Sign-up failed');
       }
       console.log('Sign-up successful');
-      // Dialogue: After successful signup we have to redirect the user to the secured page. We will do that later.
+      toast.success("signed in successful");
+     
     } catch (error) {
       console.error('Sign-up failed:', error);
+      toast.error("error while signing in");
     }
   };
 return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label className="block text-gray-700 font-semibold mb-2">Organisation Name:</label>
-        <input type="text" name="organisationName" id="organisationName" value={organisationName} onChange={(e) => setOrganisationName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
+    <div className="w-full h-screen">
+      <div className="flex flex-col">
+        <div className="min-w-[400px] min-h-[600px] border-2 border-white rounded-xl">
+          <div className="flex flex-col">
+            <label>User Name</label>
+            <input type="text" name="userName" onChange={(e)=>{setUserName(e.target.value)}}/>
+          </div>
+          <div className="flex flex-col">
+            <label>Email</label>
+            <input type="text" name="userName" onChange={(e)=>{setUserEmail(e.target.value)}}/>
+          </div>
+          <div className="flex flex-col">
+            <label>Password</label>
+            <input type="text" name="userName" onChange={(e)=>{setUserPassword(e.target.value)}}/>
+          </div>
+          <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+             Sign Up
+          </Button>
+        </div>
       </div>
-      <div>
-        <label className="block text-gray-700 font-semibold mb-2">Your Name:</label>
-        <input type="text" name="userName" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
-      </div>
-      <div>
-        <label className="block text-gray-700 font-semibold mb-2">Email:</label>
-        <input type="email" name="userEmail" id="userEmail" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
-      </div>
-      <div>
-        <label className="block text-gray-700 font-semibold mb-2">Password:</label>
-        <input type="password" name="userPassword" id="userPassword" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" />
-      </div>
-      <button type="submit" className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4">Sign up</button>
-    </form>
+    </div>
   );
 };
 
