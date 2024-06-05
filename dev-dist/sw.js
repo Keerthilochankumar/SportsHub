71ea2ec3ed1707284d06015683e8116747f98c7b
keerthilochankumar <keerthi.lochans@gmail.com>
2024-06-05 21:15:14 +0530

    made some changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ol5e0420vk8"
+    "revision": "0.428lnv7nt58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
